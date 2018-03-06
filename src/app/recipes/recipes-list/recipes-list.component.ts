45d9a9f4cca29aaf168d82fa34cb0c351a8906e4
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-05 21:47:33 -0500

    created recipe model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
+//here is where we set up the recipe list based on the recipe.modle
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+  ];
 
   constructor() { }
 
