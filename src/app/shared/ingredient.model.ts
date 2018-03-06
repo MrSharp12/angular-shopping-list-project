83149f3b3f42d5b29f8abb6433728b100b8f7201
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-03-06 08:00:58 -0500

    first stage complete

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+//this is an example of shorthand
+//because we receive the arugments, then instantly assign them
+export class Ingredient {
+    constructor ( public name: string,  public amount: number) {}
+}
